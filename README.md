3a39cc518ba102d0d9be035d929c6743fb91036a
Waterbug <amoseman18@gmail.com>
2020-10-19 16:19:29 -0600

    updated .js file, added 2nd .html file, completed .css file, completed readme file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # codeQuiz
+
+This application was quite challenging for me. The idea is to have a quiz that starts after attaching a listening event to a Start Quiz button. The questions should cycle through as the user clicks on the different choice button presented to them with each question. There will be a timer on the screen for two minutes. The less time the user completes the quiz in, the better score. At the end of the quiz they will be asked to input their initials and have them saved in the local storage of the application along with their socre.
+
+Even though this is a fun activity to bring to life, I am still struggling with pulling all the components together to have a functioning product. This is a work in progress that is very close to be wrapped up
+
+**_URL of deployed application:_**
+[Code Quiz] (https://amoseman1.github.io/codeQuiz/)
+
+**_The following image shows the web application's appearance and functionality:_**
+<img src ="./assets/screenshot_code_quiz.png"/>
--- script1.js
+++ script1.js
@@ -69,61 +69,71 @@ var questions = [
 function startQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
   questionsEl.textContent = currentQuestion.title;
-  choicesEl.innerHTML = ""; //clears out previous choices
+  choicesEl.innerHTML = "";
+  currentQuestion.choices //starts creating the choices for the first question //clears out previous choices
+    .forEach(function (choice, i) {
+      var choicebtn = document.createElement("button"); //button -> <button> </button>
+      choicebtn.setAttribute("class", "choice"); // <button class="choice"> </button>
+      choicebtn.setAttribute("value", choice); // <button class="choice" value="choice 1"> </button>
+      choicebtn.textContent = i + 1 + "." + "" + choice; // <button class="choice" value="choice 1"> 1.  choice 1</button>      where we are in index and applies choices to each button
+      choicesEl.appendChild(choicebtn);
+    });
 
-  //starts creating the choices for the first question
-  currentQuestion.choices.forEach(function (choice, i) {
-    var choicebtn = document.createElement("button"); //button -> <button> </button>
-    choicebtn.setAttribute("class", "choice"); // <button class="choice"> </button>
-    choicebtn.setAttribute("value", choice); // <button class="choice" value="choice 1"> </button>
-    choicebtn.textContent = i + 1 + "." + choice; // <button class="choice" value="choice 1"> 1.  choice 1</button>      where we are in index and applies choices to each button
-    choicesEl.appendChild(choicebtn);
-  });
-
-  //create a function that will run On click that will check 1. What user pressed w/ event object 2. the current correct anxzswer 3. are tthey equal if yes what next? if no, what next?
+  
 }
 startQuestion();
 
-document.getElementById("choices").addEventListener("click", clickEvent);
-score = 0;
-function clickEvent() {
-  for (var i = 0; i < questions.length; i++) {
-    var answer = questions[i].answer; //confirm(questions[i].title);
-    if (
-      (answer === true && questions[i].answer === "true") ||
-      (answer === false && questions[i].answer === "false")
-    ) {
-      score++;
-      alert("Correct!!");
-    } else {
-      score--;
-      alert("You are wrong!");
-      return;
-    }
+//create a function that will run On click that will check 1. What user pressed w/ event object 2. the current correct anxzswer 
+//3. are they equal if yes what next? if no, what next?
+choicesEl.addEventListener("click", clickEvent); //adding click event to go to next question
+var score = 0;
+function clickEvent(event) {
+  //var currentQuestionIndex = 0;
+  
+  var usersChoice = event.target.value;
+  //console.log(usersChoice.target);
+  var correctChoice = questions[currentQuestionIndex].answer;
+  //console.log(questions[currentQuesionIndex].answer);
+  //console.log(event.target.value);
+  currentQuestionIndex++;
+  if (this.value !== questions[currentQuestionIndex].answer) {
+  } else {
+    alert("correct!");
   }
+  //want is to cycle through the questions
+}
+clickEvent();
+
+
+var timeEl = document.querySelector("#clock");
+var secondsLeft = 90;
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      endGame();
+    }
+  }, 1000);
+}
+//need end game function
+function endGame() {
+  //stop timer, que end screen, set up text content for time to indicate highscore and initals, clear questions
 }
 
-// function clickEvent() {
-//
-//   for (var i = 0; i < questions.length; i++) {
-//     var answer = questions.answer[i];
-//     if (
-//       answer === "Ice Cream" ||
-//       answer === "Saffron" ||
-//       answer === "Paella" ||
-//       answer === "Baklava" ||
-//       answer === "Vermicilli" ||
-//       answer === "Mushroom" ||
-//       answer === "Eggs" ||
-//       answer === "Durian" ||
-//       answer === "Flour and Butter" ||
-//       answer === "Unagi"
-//     ) {
-//       alert("CORRECT!");
-//     } else {
-//       //take time off clock
-//       alert("better luck next time, you just lost 10 seconds..");
-//     }
-//     //need a function to go to next question
-//   }
-// }
+
+function saveHighScore() {
+  //last function on page
+  var newScore = {
+    score: time,
+    initials: initials,
+  };
+
+  var highScore = JSON.parse(window.localStorage.getItem("highScore")) || [];
+  highScore.push(newScore);
+  window.localStorage.setItem("highScore", JSON.stringify(highScore));
+  window.location.href = "highScore.html"; //make new htmlpage to show highscore and initials - index2.html
+}
+saveHighScore();
